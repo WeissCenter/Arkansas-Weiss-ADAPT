68952356eb8233e615cc75c6e9d821e62da31f1a
dmay-aem <dmay@vizuri.com>
2025-09-26 09:22:18 -0400

    Commit viewer env updates.

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -3,6 +3,6 @@ export const environment = {
   contentRoot: 'assets/text',
   contentFileName: 'viewer-content-text.json',
   defaultContent: 'assets/text/en/viewer-content-text.json',
-  appDomain: '',
-  API_URL: '',
+  appDomain: 'ade-viewer.adaptdata.org',
+  API_URL: 'https://4oxbz5bf9e.execute-api.us-east-1.amazonaws.com/ade-prod/',
 };
--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   envLabel: 'LOCAL',
   contentRoot: 'assets/text',
   contentFileName: 'viewer-content-text.json',
-  appDomain: 'localhost:4200',
   defaultContent: 'assets/text/en/viewer-content-text.json',
-  API_URL: '',
+  appDomain: 'ade-viewer.adaptdata.org',
+  API_URL: 'https://4oxbz5bf9e.execute-api.us-east-1.amazonaws.com/ade-prod/',
 };
