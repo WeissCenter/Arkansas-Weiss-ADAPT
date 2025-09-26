67a81698c5b583b1048e76ede286ff49ee0dddaa
dmay-aem <dmay@vizuri.com>
2025-09-26 16:36:04 -0400

    Restore production environment files.

---
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  envLabel: 'Prod',
+  contentRoot: 'assets/text',
+  contentFileName: 'viewer-content-text.json',
+  defaultContent: 'assets/text/en/viewer-content-text.json',
+  appDomain: 'ade-viewer.adaptdata.org',
+  API_URL: 'https://4oxbz5bf9e.execute-api.us-east-1.amazonaws.com/ade-prod/',
+};
