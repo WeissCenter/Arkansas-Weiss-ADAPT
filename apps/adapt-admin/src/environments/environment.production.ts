b5e81a761513023052a6e57c5e7d89ad5205df66
dmay-aem <dmay@vizuri.com>
2025-09-29 16:11:20 -0500

    Update org name.

--- apps/adapt-admin/src/environments/environment.production.ts
+++ apps/adapt-admin/src/environments/environment.production.ts
@@ -22,5 +22,5 @@ export const environment = {
   },
   loginContent: 'assets/content-labels.json',
   pagesContent: 'assets/text/admin-content-text.json',
-  organizationName: 'User Organization'
+  organizationName: 'Office of Special Education'
 };
