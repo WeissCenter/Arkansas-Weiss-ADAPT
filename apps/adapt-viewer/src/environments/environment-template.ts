0bf1f0f5c9b91d7e812eaa2021c4e5cfbf7920c2
Ben-Johan van der Walt <bvanderwalt@vizuri.com>
2025-10-24 09:20:24 -0400

    Update environment files

--- apps/adapt-viewer/src/environments/environment-template.ts
+++ apps/adapt-viewer/src/environments/environment-template.ts
@@ -1,11 +1,10 @@
 export const environment = {
   envLabel: 'Prod',
-  contentRoot: 'assets/text',
+  contentRoot: 'assets/text/generic',
   contentFileName: 'viewer-content-text.json',
   appDomain: '',
-  defaultContent: 'assets/text/en/viewer-content-text.json',
   API_URL: '',
-  logoPath: 'assets/shared/logos/states/ar',
-  logoExtension: 'png',
-  copyrightText: 'The State of Arkansas.',
+  logoPath: 'assets/shared/logos/generic',
+  logoExtension: 'svg',
+  copyrightText: 'AEM Corporation.',
 };
