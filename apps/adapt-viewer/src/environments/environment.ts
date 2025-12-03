4aa2f1d622a68536da43ff614e66c44a50db2e18
Ben-Johan van der Walt <bvanderwalt@vizuri.com>
2025-12-03 14:22:19 -0500

    Fixed a bug related to loading the viewr content

--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -1,9 +1,8 @@
 export const environment = {
   envLabel: 'LOCAL',
-  contentRoot: 'assets/text',
+  contentRoot: 'assets/text/ar',
   contentFileName: 'viewer-content-text.json',
-  defaultContent: 'assets/text/en/viewer-content-text.json',
-  appDomain: 'ose-adapt-viewer.ade.arkansas.gov',  //'ade-viewer.adaptdata.org',
+  appDomain: 'ose-adapt-viewer.ade.arkansas.gov',
   API_URL: 'https://4oxbz5bf9e.execute-api.us-east-1.amazonaws.com/ade-prod/',
   logoPath: 'assets/shared/logos/states/ar',
   logoExtension: 'png',
