b21333802e21d2fe2b262a58868cae0185f5d3d3
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-02-18 10:53:08 -0500

    add logoStyleClass to environment files

--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -6,5 +6,6 @@ export const environment = {
   API_URL: 'https://4oxbz5bf9e.execute-api.us-east-1.amazonaws.com/ade-prod/',
   logoPath: 'assets/shared/logos/states/ar',
   logoExtension: 'png',
+  logoStyleClass: 'width-card-lg',
   copyrightText: 'The State of Arkansas.',
 };
